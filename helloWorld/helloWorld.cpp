c53fbba91b31e26be124eea122b111885dac83ab
João <Joao.Neto@targetsistemas.com.br>
2022-03-12 18:02:04 -0300

    alteracoes nos exemplos de hello world para se adequarem ao readme

--- helloWorld/helloWorld.cpp
+++ helloWorld/helloWorld.cpp
@@ -6,75 +6,138 @@ using namespace std;
 // EXEMPLO DE FUNÇÃO
 void olaMundo()
 {
-    cout << "Ola Mundo!";
+    cout << "Ola Mundo dentro de uma funcao!\n\n";
 }
 
-// EXEMPLO DE FUNÇÃO COM PARAMETROS E DECLARADA ANTES DE SER IMPLEMENTADA
+// EXEMPLO DE FUNÇÃO COM PARAMETROS E DECLARADA ANTES DE SER IMPLEMENTADA A MAIN
 void funcaoComParametros(string nome, int idade);
 
 // FUNÇÃO COM PASSAGEM DE REFERÊNCIA, CALCULAREMOS BHASKARA E RETORNAREMOS 3 VALORES :)
-float funcaoComReferencia(float a, float b, float c, float &x1, float &x2);
+float calculaBhaskara(float a, float b, float c, float &x1, float &x2);
 
 // FUNÇÃO PRINCIPAL
+void alterarValorPorReferencia(int &a)
+{
+    a = 5;
+}
+
 int main()
 {
     // HELLO WORLD EM C++
-    cout << "Ola Mundo!";
+    cout << "Ola Mundo!\n\n";
 
     // EXEMPLOS DE VARIAVEIS
-    //  char charVar = 't';
-    //  int intVar = 298;
-    //  float floatVar = 49.95;
-    //  double doubleVar = 99.9999;
-    //  bool boolvar = (2 > 3);
-    //  cout << "charVar = " << charVar << endl;
-    //  cout << "intVar = " << intVar << endl;
-    //  cout << "floatVar = " << floatVar << endl;
-    //  cout << "doubleVar = " << doubleVar << endl;
-    //  cout << "boolvar = " << boolvar << endl;
+    char charVar = 't';
+    string stringVar = "String";
+    int intVar = 298;
+    float floatVar = 49.95;
+    double doubleVar = 99.9999;
+    bool boolvar = true;     // Ou igual a false
+    bool boolvar2 = (2 > 3); // Também podemos utilizar de lógicas para recermos um bool
+    cout << "charVar = " << charVar << "\n";
+    cout << "stringVar = " << stringVar << "\n";
+    cout << "intVar = " << intVar << "\n";
+    cout << "floatVar = " << floatVar << "\n";
+    cout << "doubleVar = " << doubleVar << "\n";
+    cout << "boolvar = " << boolvar << "\n";
+    cout << "boolvar2 = " << boolvar2 << "\n\n";
 
     // EXEMPLO DE USO DE FUNÇÃO
-    //  olaMundo();
+    olaMundo();
 
     // EXEMPLO DE USO DA FUNCAO DECLARADA APOS O MAIN
-    // string nome;
-    // int idade;
-    // cout << "Digite o nome: ";
-    // cin >> nome;
-    // cout << "Digite a idade: ";
-    // cin >> idade;
-    // funcaoComParametros(nome, idade);
+    string nome;
+    int idade;
+    cout << "Digite o nome: ";
+    cin >> nome;
+    cout << "Digite a idade: ";
+    cin >> idade;
+    funcaoComParametros(nome, idade);
 
-    // //EXEMPLO DE TERNÁRIO
-    // string resultado;
-    // resultado = (2 > 3) ? ("maior") : ("menor");
-    // cout << resultado;
+    string resultado;
+    // EXEMPLO DE IF
+    if (2 > 3)
+    {
+        resultado = "maior";
+    }
+    else
+    {
+        resultado = "menor";
+    }
+    // EXEMPLO DE TERNÁRIO
+    resultado = (2 > 3) ? ("maior") : ("menor");
+    cout << resultado << "\n\n";
 
+    int a[5] = {1, 2, 3, 4, 5};
+    // FOR LOOP
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << "\n";
+    }
     // EQUIVALENTE AO FOREACH
-    // int a[7];
-    // for (auto element : a)
-    // {
-    //     cout << element << endl;
-    // }
+    for (auto element : a)
+    {
+        cout << element << "\n";
+    }
+    cout << "\n\n";
+
+    // ENDEREÇOS
+    char charEndereco = 't';
+    string stringEndereco = "String";
+    int intEndereco = 298;
+    float floatEndereco = 49.95;
+    double doubleEndereco = 99.9999;
+    bool boolEndereco = true;
+    bool boolEndereco2 = (2 > 3);
+    cout << "charEndereco = " << &charEndereco << "\n";
+    cout << "stringEndereco = " << &stringEndereco << "\n";
+    cout << "intEndereco = " << &intEndereco << "\n";
+    cout << "floatEndereco = " << &floatEndereco << "\n";
+    cout << "doubleEndereco = " << &doubleEndereco << "\n";
+    cout << "boolEndereco = " << &boolEndereco << "\n";
+    cout << "boolEndereco2 = " << &boolEndereco2 << "\n\n";
+    // SIZE OF
+    cout << "charEndereco = " << sizeof(charEndereco) << "\n";
+    cout << "stringEndereco = " << sizeof(stringEndereco) << "\n";
+    cout << "intEndereco = " << sizeof(intEndereco) << "\n";
+    cout << "floatEndereco = " << sizeof(floatEndereco) << "\n";
+    cout << "doubleEndereco = " << sizeof(doubleEndereco) << "\n";
+    cout << "boolEndereco = " << sizeof(boolEndereco) << "\n";
+    cout << "boolEndereco2 = " << sizeof(boolEndereco2) << "\n\n";
+
+    // Ponteiros
+    int *p;
+    int v = 3;
+    p = &v;
+    cout << "Endereco de v = " << &v << "\n";
+    cout << "Endereco guardado em p = " << p << "\n";
 
     // LENGHT DO ARRAY
-    //  int a[7];
-    //  cout << "Length of array = " << (sizeof(a) / sizeof(*a)) << endl;
+    int b[7];
+    cout << sizeof(b) << "\n";
+    cout << sizeof(*b) << "\n";
+    cout << "Length of array = " << (sizeof(b) / sizeof(*b)) << "\n";
+
+    // EXEMPLO DE PASSAGEM POR REFERENCIA
+    int exemplo = 1;
+    cout << "exemplo:" << exemplo << "\n";
+    alterarValorPorReferencia(exemplo);
+    cout << "exemplo:" << exemplo << "\n";
 
     // EXEMPLO DE PASSAGEM POR REFERENCIA PARA CALCURAR BHASKARA
-    //  float x1 = 0, x2 = 0;
-    //  float a, b, c;
-    //  float delta;
-    //  cout << "Digite o valor de a: ";
-    //  cin >> a;
-    //  cout << "Digite o valor de b: ";
-    //  cin >> b;
-    //  cout << "Digite o valor de c: ";
-    //  cin >> c;
-    //  delta = funcaoComReferencia(a, b, c, x1, x2);
-    //  cout << "O valor do delta e: " << delta << endl;
-    //  cout << "O valor de x1 e: " << x1 << endl;
-    //  cout << "O valor de x2 e: " << x2 << endl;
+    float x1 = 0, x2 = 0;
+    float coeficienteA, coeficienteB, coeficienteC;
+    float delta;
+    cout << "Digite o valor de coeficienteA: ";
+    cin >> coeficienteA;
+    cout << "Digite o valor de coeficienteB: ";
+    cin >> coeficienteB;
+    cout << "Digite o valor de coeficienteC: ";
+    cin >> coeficienteC;
+    delta = calculaBhaskara(coeficienteA, coeficienteB, coeficienteC, x1, x2);
+    cout << "O valor do delta e: " << delta << endl;
+    cout << "O valor de x1 e: " << x1 << endl;
+    cout << "O valor de x2 e: " << x2 << endl;
 
     // EXEMPLO DE STRUCT SIMPLES
     //  struct Pessoa {
@@ -131,10 +194,11 @@ int main()
 
 void funcaoComParametros(string nome, int idade)
 {
-    cout << nome << " tem " << idade << "anos" << endl;
+    cout << nome << " tem " << idade << " anos"
+         << "\n\n";
 }
 
-float funcaoComReferencia(float a, float b, float c, float &x1, float &x2)
+float calculaBhaskara(float a, float b, float c, float &x1, float &x2)
 {
     float delta = (pow(b, 2) - 4 * a * c);
     x1 = (-b + (sqrt(delta))) / (2 * a);
